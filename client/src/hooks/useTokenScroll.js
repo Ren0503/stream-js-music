ac89bc1ffbb44588a53b7963be9bc16f8aab9e4b
Ren0503 <illuminati0440@gmail.com>
2021-07-21 23:06:51 +0700

    Add context service and utils

---
+++ client/src/hooks/useTokenScroll.js
@@ -0,0 +1,31 @@
+import React, { useCallback, useRef, useState } from 'react';
+import reqWithToken from 'services/reqWithToken';
+
+function useTokenScroll(setList, token, source) {
+    const [next, setNext] = useState(null);
+
+    const observer = useRef();
+    const lastRef = useCallback(node => {
+        if (observer.current) observer.current.disconnect();
+        observer.current = new IntersectionObserver(entries => {
+            if (entries[0].isIntersecting && next) {
+                console.log('hi');
+                const makeRequest = reqWithToken(next, token, source);
+                makeRequest()
+                    .then(response => {
+                        const data = response.data;
+                        const resultList = data.items.map(track => track.track);
+                        const next = data.next || data.playlists.next;
+                        setList(tracks => [...tracks, ...resultList]);
+                        setNext(next);
+                    })
+                    .catch(error => console.log(error));
+            }
+        })
+        if (node) observer.current.observer(node);
+    }, [next]);
+
+    return [setNext, lastRef];
+}
+
+export default useTokenScroll;
